07c023edcdd998558af7fa560123b189f6285dac
killmaster65 <77656988+killmaster65@users.noreply.github.com>
2023-12-18 21:25:05 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -4,4 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    return render_template("index.html")
+
+@app.route('/speedPage')
+def speedPage():
+    return render_template('speedPage.html')
+from flask import Flask, request, render_template
+
+
+
+
+
+
+
