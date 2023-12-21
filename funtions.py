2cd6772c3ba82690924d41cb5cb44f7431c368c2
killmaster65 <77656988+killmaster65@users.noreply.github.com>
2023-12-21 14:50:23 +0000

    all variations of speed formula.(two new funtions and updated one)

--- funtions.py
+++ funtions.py
@@ -1,4 +1,8 @@
-def getspeed():
+
+
+
+
+def getspeedByTimeDis():
   global speed
   distance = input("what is the total distance?: ")
   distance = float(distance)
@@ -8,3 +12,27 @@ def getspeed():
   print("  ")
   speed = distance/time
   print("the speed is: ",speed)
+  print("  ")
+
+def getdisBySpeedTime():
+  global distance
+  speed = input("what is the speed?: ")
+  speed = float(speed)
+  print("  ")
+  time= input("what is the time?: ")
+  time = float(time)
+  print("  ")
+  distance = speed*time
+  print("the distance is: ",distance)
+
+def gettimeByDisSpeed():
+  global time
+  distance = input("what is the total distance?: ")
+  distance = float(distance)
+  print("  ")
+  speed = input("what is the speed?: ")
+  speed = float(speed)
+  print("  ")
+  time = distance/speed
+  print("the time is: ",time)
+  print("  ")
